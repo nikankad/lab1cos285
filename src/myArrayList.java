51363a23497c7f1a5fd9aae9b2564383604954d7
Nikan <nikankad@protonmail.com>
2023-09-19 16:22:58 -0400

    init

---
+++ src/myArrayList.java
@@ -0,0 +1,20 @@
+
+
+public class myArrayList<E> {
+// made by nikan
+ private static final int INITIAL_CAPACITY = 50;
+ private static final double GROWTH_POLICY = 0.2;
+ private Object[] array;
+ private int size;
+
+ // Constructor made by nikan
+ public myArrayList() {
+     array = new Object[INITIAL_CAPACITY];
+     size = 0;
+ }
+
+ // Add methods to implement the ArrayList functionality.
+ 
+ 
+ 
+}
