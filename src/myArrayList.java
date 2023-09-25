f4f38c909fdb48a9d7bad28a2c94676d955dea55
TheHylianLegend <39139509+TheHylianLegend@users.noreply.github.com>
2023-09-25 12:59:41 -0400

    Added add method

--- src/myArrayList.java
+++ src/myArrayList.java
@@ -1,4 +1,4 @@
-
+import java.util.Arrays;
 
 public class myArrayList<E> {
 // made by nikan
@@ -15,6 +15,21 @@ public class myArrayList<E> {
 
  // Add methods to implement the ArrayList functionality.
  
- 
+ // Made by Ashley
+ void add(int index, Fraction input) {
+     if (index > size || index < 0)
+         throw new IndexOutOfBoundsException(index);
+     
+     int s;
+     Object[] array;
+     if((s = size) == (array = this.array).length) {
+         int old_capacity = array.length;
+         int new_capacity = (int) (old_capacity + (old_capacity * GROWTH_POLICY));
+         array = Arrays.copyOf(array, new_capacity);
+     }
+     System.arraycopy(array, index, array, index + 1, s - index);
+     array[index] = input;
+     size = s + 1;
+ }
  
 }
